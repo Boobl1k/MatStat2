338ba954d750b679d6745d24cd77479839c1d444
Mansur <ahmatowmansur@gmail.com>
2022-04-27 00:27:04 +0300

    q

--- main.py
+++ main.py
@@ -9,106 +9,94 @@ numbers = read_csv('telecom_churn.csv')['Total day calls'].tolist()
 
 count = len(numbers)
 
+print('Количество данных - ', len(numbers))
 print('Минимальное значение - ', min(numbers))
 print('Максимальное значение - ', max(numbers))
-print('Количество данных - ', len(numbers))
 intervalCount = math.floor(1 + 3.322 * math.log10(count))
 print('Количсетво интервалов - ', intervalCount)
 intervalLength = math.ceil((max(numbers) - min(numbers)) / math.floor(intervalCount))
 print('Длина интервала - ', intervalLength)
 
-ni = [[0 for x in range(2)] for y in range(intervalCount)]
+intervals = []
 currentLeft = min(numbers)
 for i in range(intervalCount):
-    ni[i][0] = currentLeft
-    ni[i][1] = currentLeft + intervalLength
+    intervals.append([currentLeft, currentLeft + intervalLength])
     currentLeft += intervalLength
 print('Интервалы:')
-for pair in ni:
+for pair in intervals:
     print(pair)
+
+middles = []
+for i in range(intervalCount):
+    middles.append((intervals[i][0] + intervals[i][1]) / 2)
+print('Середины интервалов:')
+print(middles)
 
 countList = []
 for i in range(intervalCount):
     x = 0
     for number in numbers:
-        if ni[i][1] >= number > ni[i][0]:
+        if intervals[i][0] < number <= intervals[i][1]:
             x += 1
     countList.append(x)
 print('Сколько попало в интервал:')
 print(countList)
 
-middles = [0.0 for y in range(0, intervalCount)]
-for i in range(len(middles)):
-    middles[i] = (ni[i][0] + ni[i][1]) / 2
-print('Середины интервалов:')
-print(middles)
-
 xMiddle = 0
 for i in range(0, intervalCount):
-    xMiddle += (middles[i] * countList[i])
-xMiddle = xMiddle / count
+    xMiddle += middles[i] * countList[i]
+xMiddle /= count
 print('Выборочная средняя - ', xMiddle)
 
-z2 = [0.0 for y in range(0, intervalCount)]
-for i in range(0, intervalCount):
-    zmid = (middles[i] - xMiddle) ** 2
-    z2[i] = zmid
-
-print(z2)
+z2 = []
+for middle in middles:
+    z2.append((middle - xMiddle) ** 2)
 
 sigma = 0.0
-for i in range(0, intervalCount):
-    sigma = sigma + z2[i] * countList[i]
-sigma = sigma / count
+for i in range(intervalCount):
+    sigma += z2[i] * countList[i]
+sigma /= count
 print('Выборочная дисперсия - ', sigma)
-
-t = createtable()
 
 y = 0
 if count <= 20:
     y = count
-elif 20 < count <= 50:
-    if count % 10 == 0:
-        y = count
-    elif count % 5 == 0:
-        y = count
-    else:
-        y = count - (count % 5)
-elif 50 < count <= 100:
-    if count % 10 == 0:
-        y = count
-    else:
-        y = count - (count % 10)
-elif 120 >= count > 100:
+elif count <= 50:
+    y = count - (count % 5)
+elif count <= 100:
+    y = count - (count % 10)
+elif count <= 120:
     y = 120
 else:
     y = 0
+
 number = 0
-for i in range(0, 29):
+t = createtable()
+for i in range(29):
     if y == t[i][0]:
         number = i
-gammaChoise = int(input('Выберите гамму из списка (1-3)\n1 - 0.95\n2 - 0,99\n3 - 0.999\n')) - 1
+gammaChoice = int(input('Выберите гамму из списка (1-3)\n1 - 0.95\n2 - 0,99\n3 - 0.999\n')) - 1
+
 gamma = 0.0
-if gammaChoise == 1:
+if gammaChoice == 1:
     gamma = 0.95
-elif gammaChoise == 2:
+elif gammaChoice == 2:
     gamma = 0.99
 else:
     gamma = 0.999
 
-xIntervalMin = xMiddle - (t[number][gammaChoise] * math.sqrt(sigma)) / math.sqrt(count)
-xIntervalMax = xMiddle + (t[number][gammaChoise] * math.sqrt(sigma)) / math.sqrt(count)
+xIntervalMin = xMiddle - (t[number][gammaChoice] * math.sqrt(sigma)) / math.sqrt(count)
+xIntervalMax = xMiddle + (t[number][gammaChoice] * math.sqrt(sigma)) / math.sqrt(count)
 
 print(xIntervalMin, '< x <', xIntervalMax)
 
-d = [0 for i in range(intervalCount)]
+d = []
 for i in range(intervalCount):
-    d[i] = middles[i] - xMiddle
+    d.append(middles[i] - xMiddle)
 
 a = 0
-
 for i in range(intervalCount):
-    a += (d[i] * d[i] * d[i] * countList[i] / count)
+    a += d[i] ** 3 * countList[i] / count
 a /= math.sqrt(sigma) ** 3
 
 df = count - 1
@@ -140,19 +128,18 @@ for zi in z2:
 m4 = 0
 for i in range(intervalCount):
     m4 += z4[i] * countList[i]
-
-m4 = m4 / count
+m4 /= count
 
 ek = m4 / (sigma ** 2) - 3
 
 print('Ek - ', ek)
 
 z3 = []
-for i in z2:
-    z3.append(i ** 1.5)
+for z2i in z2:
+    z3.append(z2i ** 1.5)
 
 m3 = 0
-for i in range(0, intervalCount):
+for i in range(intervalCount):
     m3 += z3[i] * countList[i]
 m3 /= count
 a3 = m3 / (sigma ** 1.5)
